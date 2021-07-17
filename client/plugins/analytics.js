fb8d79ff2f8aa5a13e41d52e528e54d3d15b2722
shahen94 <shahen.hovhannisyan.94@gmail.com>
2021-07-17 19:31:12 +0400

    cleaned client plugins

--- client/plugins/analytics.js
+++
@@ -1,5 +0,0 @@
-function analytics () {
-  const span = document.createElement('span');
-  span.innerHTML = 'Analytics Tab';
-  document.body.appendChild(span);
-}
--- client/plugins/avatar.js
+++
@@ -1,5 +0,0 @@
-function avatar () {
-  const span = document.createElement('span');
-  span.innerHTML = 'Avatar Tab';
-  document.body.appendChild(span);
-}
